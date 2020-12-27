53fc329f5bf100cef1ccd73b165b9777a27f70ad
Ismail Mohammed <ismailmo4@gmail.com>
2020-12-27 23:54:25 +0000

    add start script

--- app.js
+++ app.js
@@ -154,6 +154,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3000, () => {
-    console.log('listening on 3000!');
+const port=process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`listening on ${port}!`);
 });
